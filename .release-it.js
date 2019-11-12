2aaf5c2f0ff91f3b8e133abf797c6854ee2dca6c
anass <anass.oumiri@qonto.eu>
2019-11-12 14:22:43 +0100

    chore: release-it configuration

---
+++ .release-it.js
@@ -0,0 +1,20 @@
+const { GITHUB_TOKEN } = process.env;
+
+module.exports = {
+  git: {
+    tagName: 'v${version}',
+    changelog: 'yarn lerna-changelog',
+  },
+  hooks: {
+    'after:bump': 'yarn lerna-changelog',
+  },
+  npm: {
+    publish: true,
+    publishPath: '.',
+  },
+  github: {
+    release: true,
+    releaseName: 'Release ${version}',
+    tokenRef: GITHUB_TOKEN,
+  },
+};
