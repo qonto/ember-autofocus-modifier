6f6ba21fc18bc0a03f74e91dde069b696512a5d0
Davide Ferrero <davideferre@gmail.com>
2021-10-09 00:06:17 +0200

    Fixes code style

--- addon/modifiers/autofocus.js
+++ addon/modifiers/autofocus.js
@@ -2,11 +2,11 @@ import { modifier } from 'ember-modifier';
 
 const DEFAULT_SELECTOR = 'input:not([disabled]),textarea:not([disabled])';
 
-export default modifier(function autofocus(element, [selector = DEFAULT_SELECTOR], { disabled}) {
+export default modifier(function autofocus(element, [selector = DEFAULT_SELECTOR], { disabled }) {
   if (disabled) {
     return;
   }
-  
+
   const childElement = element.querySelector(selector);
 
   if (childElement) {
