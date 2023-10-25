7cd4523e71fee8026e4479002c61c10a9650fcdc
Daniele Calleri <daniele.calleri@qonto.com>
2023-10-25 16:57:09 +0200

    chore: install and use @qonto/eslint-config-typescript@1.0.0-rc.0

--- ember-autofocus-modifier/.eslintrc.cjs
+++ ember-autofocus-modifier/.eslintrc.cjs
@@ -45,67 +45,7 @@ module.exports = {
     // ts files
     {
       files: ["**/*.ts"],
-      extends: [
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-      ],
-      rules: {
-        // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended.ts
-        "@typescript-eslint/no-explicit-any": "error",
-        "@typescript-eslint/no-non-null-assertion": "error",
-        "@typescript-eslint/no-unused-vars": "error",
-        // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/strict.ts
-        "@typescript-eslint/array-type": [
-          "error",
-          {
-            default: "array",
-            readonly: "array",
-          },
-        ],
-        "@typescript-eslint/ban-tslint-comment": "error",
-        "@typescript-eslint/class-literal-property-style": "error",
-        "@typescript-eslint/consistent-generic-constructors": "error",
-        "@typescript-eslint/consistent-indexed-object-style": "error",
-        "@typescript-eslint/consistent-type-assertions": "error",
-        "@typescript-eslint/consistent-type-definitions": "error",
-        "@typescript-eslint/consistent-type-imports": "error",
-        "@typescript-eslint/explicit-function-return-type": "error",
-        "@typescript-eslint/explicit-member-accessibility": [
-          "error",
-          {
-            accessibility: "no-public",
-          },
-        ],
-        "@typescript-eslint/explicit-module-boundary-types": "error",
-        "@typescript-eslint/member-delimiter-style": "error",
-        "@typescript-eslint/member-ordering": "error",
-        "@typescript-eslint/method-signature-style": "error",
-        "@typescript-eslint/no-confusing-non-null-assertion": "error",
-        "@typescript-eslint/no-duplicate-enum-values": "error",
-        "@typescript-eslint/no-dynamic-delete": "error",
-        "@typescript-eslint/no-extraneous-class": "error",
-        "@typescript-eslint/no-import-type-side-effects": "error",
-        "@typescript-eslint/no-invalid-void-type": "error",
-        "@typescript-eslint/no-non-null-asserted-nullish-coalescing": "error",
-        "@typescript-eslint/no-require-imports": "error",
-        "@typescript-eslint/no-type-alias": [
-          "error",
-          {
-            allowGenerics: "always",
-          },
-        ],
-        "@typescript-eslint/no-unsafe-declaration-merging": "error",
-        "@typescript-eslint/parameter-properties": "error",
-        "@typescript-eslint/prefer-enum-initializers": "error",
-        "@typescript-eslint/prefer-for-of": "error",
-        "@typescript-eslint/prefer-function-type": "error",
-        "@typescript-eslint/prefer-literal-enum-member": "error",
-        "@typescript-eslint/prefer-ts-expect-error": "error",
-        "@typescript-eslint/sort-type-constituents": "error",
-        "@typescript-eslint/type-annotation-spacing": "error",
-        "@typescript-eslint/typedef": "error",
-        "@typescript-eslint/unified-signatures": "error",
-      },
+      extends: ["@qonto/eslint-config-typescript"],
     },
   ],
 };
